5edee2e95b520942f3530df25c6ce56e3c8b8f73
Tatiana Anufrieva <anufri_ta@rambler.ru>
2020-10-14 19:12:14 +0400

    Updates

---
+++ precache-manifest.115d1c1aa1394ed1cf39fe4f8b1c4449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9b678afe1a551948b264ecb96e50cd",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "64d6a751b2b895b38a5d",
+    "url": "/weather-app/static/css/main.fb6c8d5e.chunk.css"
+  },
+  {
+    "revision": "514ae3c554f8f7879f25",
+    "url": "/weather-app/static/js/2.2bfdb317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2bfdb317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d6a751b2b895b38a5d",
+    "url": "/weather-app/static/js/main.266bceef.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/weather-app/static/media/robotoslab-regular.594bcf8b.woff"
+  }
+]);
